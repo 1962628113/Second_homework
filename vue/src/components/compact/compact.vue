3db99e86b92c00be9223f7acddf4111247a76c83
qingsongwang <1962628113@qq.com>
2019-04-05 13:23:05 +0800

    '5题测试完成'

--- vue/src/components/compact/compact.vue
+++ vue/src/components/compact/compact.vue
@@ -1,18 +1,25 @@
 <template>
 <div>
   <h3>去掉数组 [1, 3, 4, null, undefined, NaN, false] 中的null、undefined、NaN</h3>
-  <div><span>数组1：</span><span>[1, 3, 4, null, undefined, NaN, false]</span></div>
-  <div><span>数组2：</span><span class="arrTwo">{{compact}}</span></div>
+  <div><span>数组1：</span><span id="arrone">{{arr1}}</span></div>
+  <button @click="compact">点击</button>
+  <div><span>数组2：</span><span id="arrtwo">{{arr2}}</span></div>
 </div>
 </template>
 <script>
 export default {
   name:'compact',
-  computed:{
-    compact: function () {
-      var arr1=[1, 3, 4, null, undefined, NaN, false]
-      var arr2=arr1.filter(item=>parseFloat(item))
-      return arr2
+  data(){
+    return {
+      arr1:[1, 3, 4, null, undefined, NaN, false],
+      arr2:''
+    }
+  },
+  methods:{
+    compact(arr) {
+      var arr=this.arr1;
+      this.arr2=arr.filter(item=>parseFloat(item))
+      return this.arr2
     }
   }
 }
--- vue/test/unit/specs/compact.spec.js
+++ vue/test/unit/specs/compact.spec.js
@@ -1,6 +1,24 @@
 import Vue from 'vue'
 import compact from '@/components/compact/compact.vue'
 
-describe('compact.vue', () => {
+function replace(str){
+  str=str.replace(/[\r\n]/g, "")
+  str=str.replace(/\s+/g, "")
+  return str
+}
+function watch(){
 
+}
+describe('compact.vue', () => {
+  it('should return a array cotain number', () => {
+    const Constructor = Vue.extend(compact)
+    const vm = new Constructor().$mount()
+    vm.arr1=[1, 3, 4, null, undefined, NaN, false,5,6]
+    const button=vm.$el.querySelector('button');
+    const clickEvent=new window.Event('click');
+    button.dispatchEvent(clickEvent);
+    vm._watcher.run();
+    const arr2=vm.$el.querySelector('#arrtwo').textContent
+    expect(replace(arr2)).toBe('[1,3,4,5,6]')
+  })
 })
