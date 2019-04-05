f6c9715e959e64b8692765813d44989bdc59f108
qingsongwang <1962628113@qq.com>
2019-04-05 11:09:03 +0800

    '5题’

---
+++ vue/src/components/compact/compact.vue
@@ -0,0 +1,19 @@
+<template>
+<div>
+  <h3>去掉数组 [1, 3, 4, null, undefined, NaN, false] 中的null、undefined、NaN</h3>
+  <div><span>数组1：</span><span>[1, 3, 4, null, undefined, NaN, false]</span></div>
+  <div><span>数组2：</span><span class="arrTwo">{{compact}}</span></div>
+</div>
+</template>
+<script>
+export default {
+  name:'compact',
+  computed:{
+    compact: function () {
+      var arr1=[1, 3, 4, null, undefined, NaN, false]
+      var arr2=arr1.filter(item=>parseFloat(item))
+      return arr2
+    }
+  }
+}
+</script>
--- vue/src/router/index.js
+++ vue/src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
 import Topfour from '@/components/Topfour/index.vue'
+import compact from '@/components/compact/compact.vue'
 Vue.use(Router)
 
 export default new Router({
@@ -16,6 +17,11 @@ export default new Router({
       path: '/Topfour',
       name: 'Topfour',
       component: Topfour
+    },
+    {
+      path: '/compact',
+      name: 'compact',
+      component: compact
     }
   ]
 })
